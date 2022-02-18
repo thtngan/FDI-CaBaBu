cae29274b6651836b40192da24ad98de46b2c039
thisislins <lephuonglinh169@gmail.com>
2022-02-18 22:34:18 +0700

    Add login

--- cov-map/src/App.js
+++ cov-map/src/App.js
@@ -4,11 +4,13 @@ import SendHelp from "./pages/SendHelp";
 import WantToHelp from "./pages/WantToHelp";
 import Header from "./components/header/Header";
 import HelpForm from "./pages/HelpForm";
+import Login from "./pages/Login";
 export default function App() {
   return (
     <div className="App">
-      <HelpForm/>
+      {/*<HelpForm/> */}
       {/* <SendHelp/> */}
+      <Login/>
     </div>
   );
 }
--- cov-map/src/pages/Login.js
+++ cov-map/src/pages/Login.js
@@ -1,5 +1,74 @@
 export default function Login(){
     return (
-        <div>Login</div>
+        <div class="font-sans">
+            <div class="relative min-h-screen flex flex-col sm:justify-center items-center bg-orange-100">
+                <div class="relative sm:max-w-sm w-full">
+                    <div class="card bg-blue-400 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
+                    <div class="card bg-orange-400 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
+                    <div class="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
+                        <label for="" class="block mt-3 text-2xl text-gray-700 text-center font-semibold">
+                            Login
+                        </label>
+                        <form method="#" action="#" class="mt-10">
+                            <input type="email" placeholder="Usename" class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"/>
+                
+                            <div class="mt-7">                
+                                <input type="password" placeholder="Password" class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"/>                           
+                            </div>
+
+                            <div class="mt-7 flex">
+                                <label for="remember_me" class="inline-flex items-center w-full cursor-pointer">
+                                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember"/>
+                                    <span class="ml-2 text-sm text-gray-600">
+                                        Remember me
+                                    </span>
+                                </label>
+                
+                               <div class="w-full text-right">     
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="#">
+                                        Forgot password?
+                                    </a>                                  
+                               </div>
+                            </div>
+                
+                            <div class="mt-7">
+                                <button class="bg-orange-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                                    Login
+                                </button>
+                            </div>
+                
+                            <div class="flex mt-7 items-center text-center">
+                                <hr class="border-gray-300 border-1 w-full rounded-md"/>
+                                <label class="block font-medium text-sm text-gray-600 w-full">
+                                    Or login with
+                                </label>
+                                <hr class="border-gray-300 border-1 w-full rounded-md"/>
+                            </div>
+                
+                            <div class="flex mt-7 justify-center w-full">
+                                <button class="mr-5 bg-blue-500 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                                    Facebook
+                                </button>
+                
+                                <button class="bg-red-500 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"> 
+                                    Google
+                                </button>
+                            </div>
+                
+                             <div class="mt-7">
+                                <div class="flex justify-center items-center">
+                                    <label class="mr-2" >You are new?</label>
+                                    <a href="#" class=" text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                                        Create new account
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
     );
 }
